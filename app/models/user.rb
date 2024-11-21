b030fa87ff6be7850a9c531d0964d2977bf98a5e
n-ira-if <ibnoaria@yahoo.co.jp>
2024-11-21 15:14:30 +0900

    ビューファイルを編集しデータ保存できたことを確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,15 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*[0-9]).{8,128}\z/
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "パスワードには半角英数字を含めてください" }, if: -> { password.present? }
+  validates :password, format: { with: VALID_PASSWORD_REGEX,}, if: -> { password.present? }
 
   VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龠々ー]+\z/
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: "全角で入力してください" }
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: "全角で入力してください" }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX}
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX}
 
   VALID_KATAKANA_REGEX = /\A[ァ-ヶー]+\z/
-  validates :first_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: "全角カタカナで入力してください" }
-  validates :last_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: "全角カタカナで入力してください" }
+  validates :first_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX}
+  validates :last_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX}
 
   validates :birth_date, presence: true
 
